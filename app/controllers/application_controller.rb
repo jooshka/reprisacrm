51ad9e9ec9351048d2cc596441885bac63cd8f14
jooshka <jooshka@mail.ru>
2017-01-01 03:24:01 +0300

    Add last_seen_at to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include Pundit
+
+  before_action :set_last_seen_at, if: proc { user_signed_in? }
+
+  private
+  def set_last_seen_at
+    current_user.update_attribute(:last_seen_at, Time.now)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161231010359) do
+ActiveRecord::Schema.define(version: 20170101001816) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20161231010359) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.datetime "last_seen_at"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
