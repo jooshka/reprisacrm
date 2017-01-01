f092e3b7be1ad359bd7405721191a518facc7a1a
jooshka <jooshka@mail.ru>
2017-01-01 03:28:47 +0300

    Add extra fields to users

---
+++ db/migrate/20170101002615_add_fields_to_users.rb
@@ -0,0 +1,9 @@
+class AddFieldsToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :patronymic, :string
+    add_column :users, :phone, :string
+    add_column :users, :active, :boolean
+  end
+end
